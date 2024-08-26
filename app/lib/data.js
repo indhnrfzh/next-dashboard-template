b3e68d9b36dde5e009bd2b5a20138db55ef38a68
indhnrfzh <105841107022@student.unismuh.ac.id>
2024-08-26 15:48:39 +0800

    selesai chapter 8

--- app/lib/data.js
+++ app/lib/data.js
@@ -9,7 +9,7 @@ export async function fetchRevenue() {
     console.log('Fetching revenue data...');
     await new Promise((resolve) => setTimeout(resolve, 3000));
 
-    const data = await sql`SELECT * FROM revenue`;
+    const data = await sql `SELECT * FROM revenue`;
 
     console.log('Data fetch completed after 3 seconds.');
 
