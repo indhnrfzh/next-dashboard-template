2d9e988589b7e6ac2cbe387b9aea84c20955377e
indhnrfzh <105841107022@student.unismuh.ac.id>
2024-08-27 16:24:07 +0800

    selesai chapter 11

--- app/ui/invoices/pagination.js
+++ app/ui/invoices/pagination.js
@@ -1,15 +1,23 @@
 'use client';
 
-import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/24/outline';
+import { ArrowLeftIcon, ArrowRightIcon, CurrencyDollarIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 import Link from 'next/link';
-import { generatePagination } from '@/app/lib/utils';
+import { generatePagination } from '../../lib/utils';
+import { usePathname, useSearchParams } from "next/navigation";
 
 export default function Pagination({ totalPages }) {
   // NOTE: Uncomment this code in Chapter 11
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const currentPage = Number(searchParams.get("page")) || 1;
+  // const allPages = generatePagination(CurrencyDollarIcon, totalPages);
 
-  // const allPages = generatePagination(currentPage, totalPages);
-
+  const createPageURL = (pageNumber) => {
+    const params = new URLSearchParams(searchParams);
+    params.set("page", pageNumber.toString());
+    return `${pathname}?${params.toString()}`;
+  };
   return (
     <>
       {/*  NOTE: Uncomment this code in Chapter 11 */}
