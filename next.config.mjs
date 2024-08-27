1a37ebac559cf7727ba638652b0693af55e003e5
indhnrfzh <105841107022@student.unismuh.ac.id>
2024-08-27 15:02:27 +0800

    selesai chapter 10

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    experimental: {
+    //   ppr: 'inc/remental',
+    },
+  };
+   
+  export default nextConfig;
